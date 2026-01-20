5e17cf75c0bb91a2749e265d0dc0135c97737fa0
imam_krytoi <imam@begemot.com>
2026-01-20 10:11:31 +0300

    Мой говоряфщий Том

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+namespace Rasul
+{
+    class Program
+    {
+       static void Main()
+        {
+            System.Console.WriteLine("Hello");
+            System.Console.WriteLine("We are watching you!!");
+        }
+    }
+}
